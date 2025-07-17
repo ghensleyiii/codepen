c1d8993100c383f351e829517208c7e115816e06
G. Hensley III <ghensleyiii@outlook.com>
2025-07-17 17:33:36 -0400

    Update background-cc.js

--- js/background-cc.js
+++ js/background-cc.js
@@ -4,6 +4,8 @@ const htmlBody = document.querySelector("body");
     const randomIndex = Math.floor(Math.random() * colors.length);
     const randomColor = colors[randomIndex];
     htmlBody.style.backgroundColor = randomColor;
+    const headOne = document.querySelector('h1');
+    headOne.textContent =`You clicked! Background is now: ${randomColor}. Click Again!`;
     console.log("You clicked! Background is now " + randomColor);
   };
   htmlBody.onclick = randomClickFunction;
